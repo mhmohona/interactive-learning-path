8dc46f31f6af90f78b6e779dd4bb097949485b32
Mahfuza Humayra Mohona <mhmohona@gmail.com>
2023-12-11 07:00:26 +0600

    setup the background file

---
+++ asyncapi/step1/background.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clone the specified repository
+git clone https://github.com/mhmohona/interactive-learning-path/tree/main/asyncapi/step1/assets
+
+# Change directory into the cloned repository
+cd interactive-learning-path/asyncapi/step1/assets
+
+# Any additional background setup tasks can be added here
